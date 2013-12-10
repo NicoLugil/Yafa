da6bf3de210b80b6896f3e9a5d008b48fd7f1d7a
NicoLugil <nico@lugil.be>
2013-12-10 19:56:48 +0100

    aa

--- python/BridgeComm.py
+++ python/BridgeComm.py
@@ -1,3 +1,6 @@
+import sys
+import string
+
 sys.path.insert(0, '/usr/lib/python2.7/bridge/') 
 from bridgeclient import BridgeClient as bridgeclient
 bc = bridgeclient()                              
@@ -7,13 +10,13 @@ class BridgeComm:
     VALUE_LEN=32
     previous_tx_id='Z'
     def send(self,command,value):
-       if previous_tx_id=='Z':
+       if BridgeComm.previous_tx_id=='Z':
           msg_id='A'
        else:
           msg_id=chr(ord(previous_tx_id) + 1)
-          msg=msg_id+(command[:COMMAND_LEN]).ljust(COMMAND_LEN)+(value[:VALUE_LEN])ljust(VALUE_LEN)
+          msg=msg_id+(command[:COMMAND_LEN]).ljust(COMMAND_LEN)+(value[:VALUE_LEN]).ljust(VALUE_LEN)
           bc.put('key_get',msg)
-       previous_tx_id=msg_id
+       BridgeComm.previous_tx_id=msg_id
 
 
 
