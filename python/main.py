6126bbda0f244c6c6b377b74451e50c310d05a5c
NicoLugil <nico@lugil.be>
2013-12-19 23:12:38 +0100

    more work - ftp-ing temperature

--- python/main.py
+++ python/main.py
@@ -2,7 +2,12 @@
 import sys    
 import time
 import string
+import private.pw
+import datetime
 from BridgeComm import BridgeComm
+from ftplib import FTP
+import StringIO
+from ftplib import FTP
 
 myComm = BridgeComm()
 
@@ -13,8 +18,26 @@ myComm = BridgeComm()
 #myComm.check_new_msg()
 
 while True:
-    time.sleep(10)
+    time.sleep(301)
+    myFileIO = StringIO.StringIO()
     myComm.send("Temp?","-")
-    if myComm.check_new_msg():
+    #if myComm.check_new_msg():
+    if myComm.wait_for_new_msg(10):
+       now=datetime.datetime.now()
+       now_str=now.strftime("%Y-%m-%d %H:%M")
+       print now_str
        print "New Temp received"
+       print myComm.read_ID,myComm.read_command,myComm.read_value
+       myFileIO.write(now_str)
+       myFileIO.write(" ")
+       myFileIO.write(myComm.read_value)
+       myFileIO.seek(0)
+       ftp=FTP("ftp.homebrew.be")
+       ftp.login(private.pw.myUser,private.pw.myPass)
+       ftp.cwd("www.homebrew.be/Yafa")
+       ftp.storlines("APPE temp.txt",myFileIO)
+       ftp.close()
+       myFileIO.close()
 
+
+print end
