e95c4eaf0f7b569a9a323b3519708629193b26b6
NicoLugil <nico@lugil.be>
2014-07-18 12:08:06 +0200

    exception will send mail

--- python/SendMail.py
+++ python/SendMail.py
@@ -3,20 +3,21 @@ import smtplib
 from email.mime.text import MIMEText
 import private.pw
 
-USERNAME = private.pw.myMailUser
-PASSWORD = private.pw.myMailPass
-MAILTO  = private.pw.myMailUser #private.pw.myMailTo
-
-msg = MIMEText('2nd mail Hello,\nMy name is Yun, \nhow are you')
-msg['Subject'] = 'Mail from Yun 2'
-msg['From'] = USERNAME
-msg['To'] = MAILTO
-
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo_or_helo_if_needed()
-server.starttls()
-server.ehlo_or_helo_if_needed()
-server.login(USERNAME,PASSWORD)
-server.sendmail(USERNAME, MAILTO, msg.as_string())
-server.quit()
-
+def SendMail(tto, subject, body):
+    USERNAME = private.pw.myMailUser
+    PASSWORD = private.pw.myMailPass
+    
+    #msg = MIMEText('2nd mail Hello,\nMy name is Yun, \nhow are you')
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = USERNAME
+    msg['To'] = tto
+    
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo_or_helo_if_needed()
+    server.starttls()
+    server.ehlo_or_helo_if_needed()
+    server.login(USERNAME,PASSWORD)
+    server.sendmail(USERNAME, tto, msg.as_string())
+    server.quit()
+    
