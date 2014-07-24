c90f2987ff30f1a617f597fb340601c1601d5a21
NicoLugil <nico@lugil.be>
2014-07-24 19:12:09 +0200

    wifi stuff

--- python/wifiMonitor.py
+++ python/wifiMonitor.py
@@ -31,6 +31,11 @@ import glob,os
 from TimedActions import TimedActions
 import lib.pythonping
 
+LOGFILE="/tmp/wifiMonitor"
+LOGFILESD="/mnt/sda1/arduino/wifiMonitor_"
+RESTART_WIFI_WHEN_LOST = True
+TO_PING = "192.168.1.1"
+
 def now():
       now_t=datetime.datetime.now()
       now_str=now_t.strftime("%Y-%m-%d %H:%M")
@@ -38,7 +43,7 @@ def now():
 
 def copylog():
       # only copy last part
-      copyfile("/tmp/wifiMonitor.log","/mnt/sda1/arduino/wifiMonitor_"+now()+".log")
+      copyfile(LOGFILE+".log",LOGFILESD+now()+".log")
 
 def wifi_down_up():
       call(["wifi","down"], stdout=open(os.devnull, 'wb'))
@@ -47,8 +52,10 @@ def wifi_down_up():
       time.sleep(5)
 
 def runit():
+
+  print "log will be written to" + LOGFILE + ".log and backed up at " + LOGFILESD + "<time>.log"
      
-  filelist=glob.glob('/tmp/wifiMonitor.*') 
+  filelist=glob.glob(LOGFILE+"*") 
   for f in filelist: 
      #print "removing " + str(f)
      os.remove(f) 
@@ -56,30 +63,33 @@ def runit():
 
   my_logger = logging.getLogger('MyLogger')
   my_logger.setLevel(logging.DEBUG)
-  handler = logging.handlers.RotatingFileHandler("/tmp/wifiMonitor.log", maxBytes=4096, backupCount=4)
+  handler = logging.handlers.RotatingFileHandler(LOGFILE+".log", maxBytes=16384, backupCount=2)
   my_logger.addHandler(handler)
 
-  timer_checkwifi = TimedActions(61)  # check every 1+ min 
+  timer_checkwifi = TimedActions(16)  # check every ... sec
   timer_cp2SD = TimedActions(900) # copy last part of log to SD every 15 min
 
   n_ok=0
   n_err=0
-  starttime=datetime.datetime.now()
 
   while True:
     try:
 
       wifi_down_up()
-      my_logger.debug("{0} : wifiMonitor (re)started".format(now()))
+      time.sleep(15)  # give it some more time to establish connection 
+      my_logger.debug("{0} : wifiMonitor (re)started with RESTART_WIFI_WHEN_LOST={1}".format(now(),RESTART_WIFI_WHEN_LOST))
+      starttime=datetime.datetime.now()
 
       while True:
          time.sleep(5)
          if timer_checkwifi.enough_time_passed():
-             ping_delay = lib.pythonping.do_one("192.168.1.1",5)  
+             ping_delay = lib.pythonping.do_one(TO_PING,5)  
              if ping_delay is None:
                   n_err=n_err+1
                   my_logger.debug("{0} : wifi lost".format(now()))
-                  wifi_down_up()
+                  if RESTART_WIFI_WHEN_LOST:
+                      wifi_down_up()
+                      my_logger.debug("{0} :    restarted wifi".format(now()))
                   time.sleep(15)  # give it some more time to establish connection 
                   starttime=datetime.datetime.now()
              else:
@@ -92,7 +102,6 @@ def runit():
       my_logger.debug("--- Exception caught ---")
       template = "{2} : An exception of type {0} occured. Arguments:\n{1!r}"
       message = template.format(type(e).__name__, e.args, now())
-      print message
       my_logger.debug(str(message))
       my_logger.debug("----")
       copylog()                     
